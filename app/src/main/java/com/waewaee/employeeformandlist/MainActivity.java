42b6ecd67c01bfcb7c9bc6d07cf962e7fccb73b4
Wae Waee <waewaee@gmail.com>
2022-05-23 23:39:06 +0630

    initial commit

---
+++ app/src/main/java/com/waewaee/employeeformandlist/MainActivity.java
@@ -0,0 +1,30 @@
+package com.waewaee.employeeformandlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Menu;
+import com.waewaee.employeeformandlist.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Employee Form");
+        toolbar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+}
